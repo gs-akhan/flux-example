c0c24b0c62086535d3985deca7dfd96a42e913cc
gs-akhan <akhan@gainsight.com>
2015-12-21 11:59:48 +0530

    Upgraded React to 0.14

--- js/app.js
+++ js/app.js
@@ -8,10 +8,11 @@
  */
 
 var React = require('react');
-
+var ReactDOM = require('react-dom');
 var TodoApp = require('./components/TodoApp.react');
 
-React.render(
+console.log(ReactDOM)
+ReactDOM.render(
   <TodoApp />,
   document.getElementById('todoapp')
 );
